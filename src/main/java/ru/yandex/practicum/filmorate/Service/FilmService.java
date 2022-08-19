d9ea0d470e59e3096b32be92d1f49026e311073e
Konkovmax <konkovmax@yandex.ru>
2022-08-19 09:37:25 +0300

    Version 0.2.0 users&friends

--- src/main/java/ru/yandex/practicum/filmorate/Service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/Service/FilmService.java
@@ -1,11 +1,17 @@
 package ru.yandex.practicum.filmorate.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
 public class FilmService {
     private final InMemoryFilmStorage filmStorage;
@@ -15,5 +21,39 @@ public class FilmService {
     public FilmService(InMemoryFilmStorage filmStorage) {
         this.filmStorage = filmStorage;
         films = filmStorage.films;
+    }
+
+    private Comparator<Film> comparator = Comparator.comparingInt(t -> t.getLikes().size());
+
+    public Set<Film> sortedFilms = new TreeSet<>(comparator);
+
+    public void addLike(int filmId, int userId) {
+        if (!films.containsKey(filmId)) {
+            log.warn("Film not found");
+            throw new NotFoundException(String.format(
+                    "Film with id: %s not found",
+                    filmId));
+        } else {
+            films.get(filmId).addLike(userId);
+
+        }
+    }
+
+    public void removeLike(int filmId, int userId) {
+        if (films.containsKey(filmId)) {
+            films.get(filmId).removeLike(userId);
+        }
+    }
+
+    public List<Film> getPopular(int count) {
+        sortedFilms = films.values().stream().collect(Collectors.toSet());
+        List<Film> popFilms = new ArrayList<>();
+        int j = 0;
+        Iterator<Film> i = sortedFilms.iterator();
+        while (i.hasNext() & j < count) {
+            j++;
+            popFilms.add(i.next());
+        }
+        return popFilms;
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -43,4 +43,6 @@ public class FilmController {
      filmStorage.update(film);
         return film;
     }
+
+    @GetMapping("")
 }
