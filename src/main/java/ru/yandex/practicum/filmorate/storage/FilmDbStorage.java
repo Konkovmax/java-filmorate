62189b81f99219b22880811dc62a6b6ebe0a4729
Sergey <sleontiev777@gmail.com>
2022-10-04 21:11:22 +0300

    refactor

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -21,11 +22,14 @@ import java.util.*;
 public class FilmDbStorage implements FilmStorage {
 
     private final GenreDbStorage genreStorage;
+    private final DirectorDbStorage directorStorage;
     private final JdbcTemplate jdbcTemplate;
 
-    public FilmDbStorage(JdbcTemplate jdbcTemplate, GenreDbStorage genreStorage) {
+    @Autowired
+    public FilmDbStorage(JdbcTemplate jdbcTemplate, GenreDbStorage genreStorage, DirectorDbStorage directorStorage) {
         this.jdbcTemplate = jdbcTemplate;
         this.genreStorage = genreStorage;
+        this.directorStorage = directorStorage;
     }
 
     public Film create(Film film) {
@@ -53,6 +57,9 @@ public class FilmDbStorage implements FilmStorage {
         for (Genre genre : film.getGenres()) {
             jdbcTemplate.update(createQuery, genre.getId(), filmId);
         }
+        directorStorage.updateDirectorsFromFilm(film);
+
+
         log.info("Film added");
         return film;
     }
@@ -70,6 +77,7 @@ public class FilmDbStorage implements FilmStorage {
             createQuery = "delete from FILMS_GENRES where  filmid = ? ";
             jdbcTemplate.update(createQuery, film.getId());
             String createQuery2 = "insert into FILMS_GENRES(genreid, filmid) values (?, ?)";
+            directorStorage.updateDirectorsFromFilm(film);
             for (Genre genre : film.getGenres()) {
                 try {
                     jdbcTemplate.update(createQuery2, genre.getId(), film.getId());
@@ -141,6 +149,7 @@ public class FilmDbStorage implements FilmStorage {
                 resultSet.getString("mpaName"));
         List<Genre> genres = genreStorage.getFilmsGenre(film.getId());
         film.setGenres(genres);
+        film.setDirectors(directorStorage.getDirectorsFromFilm(film));
         return film;
     }
 
