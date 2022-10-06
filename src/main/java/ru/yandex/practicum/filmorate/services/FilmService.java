9952ad81aab7b520a5491836d0cd2cf52f6f7139
Sveta2022 <lls2007@yandex.ru>
2022-10-06 18:51:41 +0300

    feat: добавить Популярность фильмов

--- src/main/java/ru/yandex/practicum/filmorate/services/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/services/FilmService.java
@@ -4,12 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
-import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.EventDbStorage;
-import ru.yandex.practicum.filmorate.storage.UserDbStorage;
 import ru.yandex.practicum.filmorate.storage.FilmDbStorage;
+import ru.yandex.practicum.filmorate.storage.UserDbStorage;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,14 +17,13 @@ import java.util.stream.Collectors;
 public class FilmService {
     private final FilmDbStorage filmStorage;
     private final UserDbStorage userStorage;
-    private final EventDbStorage eventStorage;
 
 
     @Autowired
-    public FilmService(FilmDbStorage filmStorage, UserDbStorage userStorage, EventDbStorage eventStorage) {
+    public FilmService(FilmDbStorage filmStorage, UserDbStorage userStorage) {
         this.filmStorage = filmStorage;
         this.userStorage = userStorage;
-        this.eventStorage = eventStorage;
+
     }
 
     public Film create(Film film) {
@@ -49,14 +47,6 @@ public class FilmService {
         }
     }
 
-    public void delete(int filmId) {
-        var deletedFilm = filmStorage.delete(filmId);
-        if (!deletedFilm) {
-            throw new NotFoundException(String.format(
-                    "Film with id: %s not found", filmId));
-        }
-    }
-
     public List<Film> findAll() {
         return filmStorage.findAll();
     }
@@ -72,9 +62,6 @@ public class FilmService {
 
     public void addLike(int filmId, int userId) {
         filmStorage.addLike(filmId, userId);
-        Event likeEvent = new Event(userId, "LIKE", "ADD");
-        likeEvent.setEntityId(filmId);
-        eventStorage.addEvent(likeEvent);
     }
 
     public void removeLike(int filmId, int userId) {
@@ -85,14 +72,38 @@ public class FilmService {
                     userId));
         } else {
             filmStorage.removeLike(filmId, userId);
-            Event likeEvent = new Event(userId, "LIKE", "REMOVE");
-            likeEvent.setEntityId(filmId);
-            eventStorage.addEvent(likeEvent);
         }
     }
 
-    public List<Film> getPopular(int count) {
-        return filmStorage.getPopular(count);
+    public List<Film> getPopular(Integer year, Integer genreId, Integer count) {
+        if(year == null && genreId == null){
+            return filmStorage.getPopular(count);
+        }
+        else if (year == null) {
+            //Метод по жанрам
+            return filmStorage.getPopularByGenre(genreId, count);
+        } else if (genreId == null) {
+            //Метод по годам
+            return filmStorage.getPopularByYear(year, count);
+        } else {
+            //Метод по годам и жанрам
+            return filmStorage.getPopularByGenreAndYear(year, genreId, count);
+        }
+    }
+
+
+    public List<Film> getCommonFilms(Long userId, Long friendId) {
+        return getSortedFilms(filmStorage.getCommonFilms(userId, friendId));
+    }
+
+    private List<Film> getSortedFilms(List<Film> films) {
+        return films.stream().sorted((film0, film1) -> {
+            Integer likeFilm0Size = film0.getLikes().size();
+            Integer likeFilm1Size = film1.getLikes().size();
+            int comp = likeFilm1Size.compareTo(likeFilm0Size);
+            return comp;
+        }).collect(Collectors.toList());
+
     }
 
     public List<Film> getFilmsDirectorSortedByLike(int directorId) {
@@ -102,11 +113,4 @@ public class FilmService {
     public List<Film> getFilmsDirectorSortedByYears(int directorId) {
         return filmStorage.getFilmsDirectorSortedByYears(directorId);
     }
-    
-    public List<Film> search(String query, String params) {
-        String [] items = params.split(",");
-        List<String> searchParam = Arrays.asList(items);
-        return getSortedFilms(filmStorage.search(query, searchParam));
-
-    }   
 }
