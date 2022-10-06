837fec72c7039697f4212f55f515a375d957f80d
Sveta2022 <lls2007@yandex.ru>
2022-10-06 07:48:54 +0300

    Feat: add recommendations

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/RecommendationDBStorage.java
@@ -0,0 +1,48 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RecommendationDBStorage {
+
+
+    private final JdbcTemplate jdbcTemplate;
+    private final FilmDbStorage filmStorage;
+
+    public List<Film> recommendations(int userId) {
+
+        String createQuery = "select USERSID from(select USERSID, COUNT(FILMID) as commonFilm " +
+                "from LIKES " +
+                "WHERE USERSID != ? " +
+                "AND FILMID IN (select FILMID from LIKES Where USERSID = ?) " +
+                "GROUP BY USERSID " +
+                "ORDER BY commonFilm DESC " +
+                "LIMIT 1) ";
+
+        int otherUserId = jdbcTemplate.queryForList(createQuery, Integer.class, userId, userId).get(0);
+
+        String createQueryRecommendation = "select FILMID from LIKES " +
+                "WHERE USERSID = ? AND " +
+                "FILMID NOT IN (select FILMID from LIKES Where USERSID = ?) ";
+
+        List<Integer> filmsId = jdbcTemplate.queryForList(createQueryRecommendation, Integer.class, otherUserId, userId);
+        List<Film> recommendations = new ArrayList<>();
+        for (int idFilm : filmsId) {
+            Optional<Film> film = filmStorage.getFilm(idFilm);
+            if (film.isPresent()) {
+                recommendations.add(filmStorage.getFilm(idFilm).get());
+            }
+        }
+        return recommendations;
+    }
+}
