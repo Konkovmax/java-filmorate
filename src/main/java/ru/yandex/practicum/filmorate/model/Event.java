fc61492b2f6f42bf206221247807bd4233f3c21e
samoylenko <samoylenko@lanit.ru>
2022-10-09 02:40:58 +0300

    feat: add functionality (controller, storage, service, model) for events

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Event.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+@Data
+public class Event {
+    int eventId;
+    int userId;
+    String eventType;
+    String operation;
+    int entityId;
+
+    public Event(int userId, String eventType, String operation) {
+        this.userId = userId;
+        this.eventType = eventType;
+        this.operation = operation;
+    }
+}
