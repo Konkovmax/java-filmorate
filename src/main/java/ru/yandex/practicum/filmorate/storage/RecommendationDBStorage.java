3ef447e86444713210036f0fbff810837b1b3b3e
Svetlana Yakovleva <lls2007@yandex.ru>
2022-10-11 16:29:21 +0300

    fix: исправлен метод рекоммендаций с пользователем без лайков

--- src/main/java/ru/yandex/practicum/filmorate/storage/RecommendationDBStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/RecommendationDBStorage.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.ArrayList;
@@ -19,8 +20,13 @@ public class RecommendationDBStorage {
     private final FilmDbStorage filmStorage;
 
     public List<Film> recommendations(int userId) {
-
-        String createQuery = "select USERSID from(select USERSID, COUNT(FILMID) as commonFilm " +
+        List<Film> recommendations = new ArrayList<>();
+        String createQuery = "select FILMID from LIKES Where USERSID = ? ";
+        List<Integer> filmsForUserId = jdbcTemplate.queryForList(createQuery, Integer.class, userId);
+        if (filmsForUserId.size()<1) {
+            return recommendations;
+        }
+        createQuery = "select USERSID from(select USERSID, COUNT(FILMID) as commonFilm " +
                 "from LIKES " +
                 "WHERE USERSID != ? " +
                 "AND FILMID IN (select FILMID from LIKES Where USERSID = ?) " +
@@ -28,14 +34,18 @@ public class RecommendationDBStorage {
                 "ORDER BY commonFilm DESC " +
                 "LIMIT 1) ";
 
-        int otherUserId = jdbcTemplate.queryForList(createQuery, Integer.class, userId, userId).get(0);
+        List <Integer> otherUserIds = jdbcTemplate.queryForList(createQuery, Integer.class, userId, userId);
+        if (otherUserIds.size()<1) {
+          throw new NotFoundException("Пользователь с похожими лайками фильмов не найден");
+        }
+        int otherUserId = otherUserIds.get(0);
 
         String createQueryRecommendation = "select FILMID from LIKES " +
                 "WHERE USERSID = ? AND " +
                 "FILMID NOT IN (select FILMID from LIKES Where USERSID = ?) ";
 
         List<Integer> filmsId = jdbcTemplate.queryForList(createQueryRecommendation, Integer.class, otherUserId, userId);
-        List<Film> recommendations = new ArrayList<>();
+
         for (int idFilm : filmsId) {
             Optional<Film> film = filmStorage.getFilm(idFilm);
             if (film.isPresent()) {
